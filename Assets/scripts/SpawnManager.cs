987dc190fd3c76722bb0db1d5907190b632a8597
Muhammadnur8 <250523@tmuni.org>
2025-11-17 21:17:50 +0500

    nurik

---
+++ Assets/scripts/SpawnManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    [SerializeField]
+    private GameObject[] powerups;
+    
+    private bool _stopSpawning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
+    }
+    IEnumerator SpawnEnemyRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7f, 0f);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+    IEnumerator SpawnPowerupRoutine ()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            int randomPowerUp = Random.Range(0, 3);
+            Instantiate(powerups[randomPowerUp], posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(3.0f, 8.0f));
+        }
+    }
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+}
